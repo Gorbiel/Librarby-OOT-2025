1a68a275c8a927dd632c53796b3d0869a53c3d21
Gabriel Kania <gracz1212yt@gmail.com>
2025-12-02 12:31:12 +0100

    First commmit, project structure

---
+++ src/main/java/agh/oot/librarby/LibrarbyApplication.java
@@ -0,0 +1,13 @@
+package agh.oot.librarby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/agh/oot/librarby/LibrarbyApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.oot.librarby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
