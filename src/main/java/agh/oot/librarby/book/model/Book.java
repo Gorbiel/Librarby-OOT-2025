30f1a41a9b410aec3434912f2e480dfd8ba6d62a
tomaszmol <tmollegro@gmail.com>
2025-12-06 16:21:47 +0100

    fix: Remove unused validation annotations from Book.java

--- src/main/java/agh/oot/librarby/book/model/Book.java
+++ src/main/java/agh/oot/librarby/book/model/Book.java
@@ -2,8 +2,6 @@ package agh.oot.librarby.book.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.HashSet;
 import java.util.Set;
