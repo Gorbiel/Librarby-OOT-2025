8e842b44794377743701200c7ad55f4e28f8e053
Gabriel Kania <gracz1212yt@gmail.com>
2025-12-08 15:56:04 +0100

    add CRUD user update (PATCH)

--- src/main/java/agh/oot/librarby/user/model/controller/UserController.java
+++ src/main/java/agh/oot/librarby/user/model/controller/UserController.java
@@ -1,12 +1,8 @@
 package agh.oot.librarby.user.model.controller;
 
-import agh.oot.librarby.user.model.UserAccount;
 import agh.oot.librarby.user.model.dto.UserDto;
 import agh.oot.librarby.user.model.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1/readers")
@@ -17,8 +13,14 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @GetMapping(value = "/{userId}")
     public UserDto getUserById(@PathVariable("userId") Long userAccountId) {
         return userService.getUserAccount(userAccountId);
+    }
+
+    @PatchMapping(value = "/{userId}")
+    public UserDto updateUserById(@PathVariable("userId") Long userAccountId, @RequestBody UserDto userDto) {
+        return userService.updateUserAccount(userAccountId, userDto);
     }
 }
